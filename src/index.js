7adf6f8dcf82121f06e34f786124b48505771d6b
Pratyush Singh <singh.pratyush96@gmail.com>
2017-11-09 19:24:50 +0530

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import Root from './root';
--- src/root.js
+++ src/root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Provider} from 'react-redux';
-import {Route, BrowserRouter, Link} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 import Header from './components/header/Header';
 
