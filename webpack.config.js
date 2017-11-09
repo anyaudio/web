ea43c4de35c4dc755844924f913402caa4d59416
Pratyush Singh <singh.pratyush96@gmail.com>
2017-11-09 15:07:25 +0530

    Eject app and add babel

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['transform-class-properties']
+        }
+      }
+    ]
+  }
+};
