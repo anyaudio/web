fe0c0065a3da376b0fc67251925826812afc28d4
Pratyush Singh <singh.pratyush96@gmail.com>
2017-11-11 17:55:33 +0530

    Improve search cards

--- src/components/navigation/videoCard/list/videoCardList.js
+++ src/components/navigation/videoCard/list/videoCardList.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
+
+import './static/css/videoCardList.css';
 
 export default class VideoCardList extends React.Component {
   render() {
     return (
-      <div>
-        {this.props.video.title}
+      <div className='video-card-list row'>
+        <img src={this.props.video.thumb} className='img-responsive img-circle video-card-list-thumb' alt='IMG'/>
+        <div className='video-card-list-details col-xs-11 col-sm-11 col-md-10 col-lg-8'>
+          <div className='video-card-list-time'>
+            {this.props.video.length}
+          </div>
+          <div className='video-card-list-title'>
+            {this.props.video.title}
+          </div>
+          <div className='video-card-list-uploader'>
+            {this.props.video.uploader}
+          </div>
+          <div className='video-card-list-views'>
+            {this.props.video.views}
+          </div>
+        </div>
+        <FontAwesome name='plus'/>
       </div>
     )
   }
--- src/components/navigation/videoCard/list/videoCardListList.js
+++ src/components/navigation/videoCard/list/videoCardListList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import VideoCardList from './videoCardList';
+import './static/css/videoCardList.css';
 
 export default class VideoCardListList extends React.Component {
   render() {
     return (
-      <div>
+      <div className='video-card-list-list'>
         {this.props.videos && this.props.videos.map(video => <VideoCardList video={video} key={video.id} />)}
       </div>
     )
