01f25e6df0dc4baa32f5bbfad58a1e6738d92f70
Pratyush Singh <singh.pratyush96@gmail.com>
2017-11-13 21:25:09 +0530

    Update service worker logic

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -56,7 +56,7 @@ function registerValidSW(swUrl) {
               // the fresh content will have been added to the cache.
               // It's the perfect time to display a "New content is
               // available; please refresh." message in your web app.
-              console.log('New content is available; please refresh.');
+              alert('New content is available; please refresh.');
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
