48a81ac571993ab30175df9e93d50dad82a74374
Pratyush Singh <singh.pratyush96@gmail.com>
2017-11-19 14:47:50 +0530

    Change header brand

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -18,10 +18,7 @@ export default class Header extends React.Component {
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to='/'>[LOGO] </Link>
-            <span className='hidden-sm hidden-xs'>
-              <Link to='/'>AnyAudio</Link>
-            </span>
+            g<Link to='/'>AnyAudio</Link>
           </Navbar.Brand>
         </Navbar.Header>
         <Navbar.Form pullLeft>
