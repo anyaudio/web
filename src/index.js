e4f6e71825b95f9ca986a5afd6c13b0df72bad6c
ansarimofid <ansarimofid@gmail.com>
2017-12-04 16:41:23 +0530

    Imports UIKIT components

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import Root from './root';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
 
+import Icons from 'uikit/dist/js/uikit-icons';
+import UIkit from 'uikit';
+
+import 'uikit/dist/css/uikit.min.css'
+
+UIkit.use(Icons);
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
