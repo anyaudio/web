27ad6a0db7e48f1868b9357928f6333aa0e73019
Pratyush Singh <singh.pratyush96@gmail.com>
2018-01-09 14:36:02 +0530

    Improve player

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import plyr from 'plyr';
+
+import 'plyr/dist/plyr.css';
 
 import api from '../../utils/api';
 import notify from '../../utils/notification';
@@ -11,6 +14,52 @@ export default class Player extends React.Component {
   }
 
   componentDidMount() {
+
+    plyr.setup({
+      html: [
+        "<div class='plyr__controls'>",
+          "<span class='plyr__progress'>",
+            "<label for='seek{id}' class='plyr__sr-only'>Seek</label>",
+            "<input id='seek{id}' class='plyr__progress--seek' type='range' min='0' max='100' step='0.1' value='0' data-plyr='seek'>",
+            "<progress class='plyr__progress--played' max='100' value='0' role='presentation'></progress>",
+            "<progress class='plyr__progress--buffer' max='100' value='0'>",
+              "<span>0</span>% buffered",
+            "</progress>",
+            "<span class='plyr__tooltip'>00:00</span>",
+          "</span>",
+          "<button type='button' data-plyr='play'>",
+            "<svg><use xlink:href='#plyr-play'></use></svg>",
+            "<span class='plyr__sr-only'>Play</span>",
+          "</button>",
+          "<button type='button' data-plyr='pause'>",
+            "<svg><use xlink:href='#plyr-pause'></use></svg>",
+            "<span class='plyr__sr-only'>Pause</span>",
+          "</button>",
+          "<button type='button' id='next-btn'>",
+            "<svg><use xlink:href='#plyr-fast-forward'></use></svg>",
+            "<span class='plyr__sr-only'>Forward {seektime} secs</span>",
+          "</button>",
+          "<span class='plyr__time'>",
+            "<span class='plyr__sr-only'>Current time</span>",
+            "<span class='plyr__time--current'>00:00</span>",
+          "</span>",
+          "<span class='plyr__time'>",
+            "<span class='plyr__sr-only'>Duration</span>",
+            "<span class='plyr__time--duration'>00:00</span>",
+          "</span>",
+          "<button type='button' data-plyr='mute'>",
+            "<svg class='icon--muted'><use xlink:href='#plyr-muted'></use></svg>",
+            "<svg><use xlink:href='#plyr-volume'></use></svg>",
+            "<span class='plyr__sr-only'>Toggle Mute</span>",
+          "</button>",
+          "<span class='plyr__volume'>",
+            "<label for='volume{id}' class='plyr__sr-only'>Volume</label>",
+            "<input id='volume{id}' class='plyr__volume--input' type='range' min='0' max='10' value='5' data-plyr='volume'>",
+            "<progress class='plyr__volume--display' max='10' value='0' role='presentation'></progress>",
+          "</span>",
+        "</div>"].join("")
+  });
+
     let audioElement = document.getElementById('audio-player');
     if (!audioElement) {
       return;
@@ -67,6 +116,14 @@ export default class Player extends React.Component {
         }
       }
     });
+
+    // Next on button click
+    let nextButton = document.getElementById('next-btn');
+    if (nextButton) {
+      nextButton.onclick = (e) => {
+        this.props.playNext();
+      }
+    }
   }
 
   componentWillReceiveProps(newProps) {
@@ -82,13 +139,13 @@ export default class Player extends React.Component {
   render() {
     return (
       <div>
-        <div>
+        <div style={{display: this.state.streamURL ? 'block' : 'none'}}>
           <audio id='audio-player' src={this.state.streamURL} autoPlay={true} controls={true} preload='auto'>
             Audio not supported!!
           </audio>
-          {this.props.currentSong && this.props.currentSong.title}
-          <button onClick={this.props.playNext}>Next</button>
         </div>
+        {this.props.currentSong && this.props.currentSong.title}
+        <button onClick={this.props.playNext}>Next</button>
       </div>
     )
   }
