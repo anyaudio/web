22bf4aaffee9f7300a79d295b41eb050885c6652
Pratyush Singh <singh.pratyush96@gmail.com>
2018-01-19 11:06:17 +0530

    Auto reload on new version

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -56,7 +56,7 @@ function registerValidSW(swUrl) {
               // the fresh content will have been added to the cache.
               // It's the perfect time to display a "New content is
               // available; please refresh." message in your web app.
-              alert('New content is available; please refresh.');
+              location.reload(true);
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
