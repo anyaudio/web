30605fbd0b9c85ef6a17a7e0c90c72cb891d4241
Anshuman Verma <anshu.av97@gmail.com>
2018-02-07 11:06:41 +0530

    Spacing indentation in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 export const keys = {
-	SPACE: 32,
-	N: 78,
-	R: 82,
-	F: 70,
-	L: 76,
-	LEFT_ARROW: 37,
-	UP_ARROW: 38,
-	RIGHT_ARROW: 39,
-	DOWN_ARROW: 40
+  SPACE: 32,
+  N: 78,
+  R: 82,
+  F: 70,
+  L: 76,
+  LEFT_ARROW: 37,
+  UP_ARROW: 38,
+  RIGHT_ARROW: 39,
+  DOWN_ARROW: 40
 }
