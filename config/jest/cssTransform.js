353f2bd8fc8f9c371eaecee0dcf4afc6065b1369
ANKUSH SINGH <97472714+ankush788@users.noreply.github.com>
2023-01-04 02:38:18 +0530

    important

--- config/jest/cssTransform.js
+++ config/jest/cssTransform.js
@@ -2,7 +2,7 @@
 
 // This is a custom Jest transformer turning style imports into empty objects.
 // http://facebook.github.io/jest/docs/tutorial-webpack.html
-
+// new comment for project
 module.exports = {
   process() {
     return 'module.exports = {};';
